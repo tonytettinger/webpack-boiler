915641afe82ac392d245193075ffbeb3d8135d97
tonytettinger <antal.tetinger@gmail.com>
2022-01-04 09:41:21 +0100

    Adding analyzer to check bundle sizes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,11 @@
 const {merge} = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
-
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: 'bundle_sizes.html'
+  })]
 })
