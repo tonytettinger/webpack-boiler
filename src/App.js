b39506a0d3de00ad2b88b1ae9bfd344b89e59932
tonytettinger <antal.tetinger@gmail.com>
2022-01-04 09:29:54 +0100

    Adding react hot reloader dev option

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import {hot} from 'react-hot-loader'
 const App = () => {
 	return(
 		<h1>Hello I'm a title!!</h1>
 	)
 }
 
-export default App
+export default hot(module)(App)
